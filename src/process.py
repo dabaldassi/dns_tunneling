dff04d0932a6ea8ad2758bdcd8eeb3c533330a97
dabaldassi <david.baldassin@etu.uca.fr>
2019-03-13 11:32:13 +0100

    comment process.py

--- src/process.py
+++ src/process.py
@@ -1,18 +1,28 @@
 from queue import Queue
 from threading import Thread
 
+"""
+class Process
+Wrapper of a subprocess to read the process output in non blocking mode
+"""
+
 class Process:
 
     def __init__(self, p):
         self.endProcess = False # if the process is ended
         self.end = False # If the process is ended and there is no more reading
         self.p = p
-        self.queue = Queue()
-        self.thread = Thread(target=self.run)
+        self.queue = Queue() # Queue to store data
+        self.thread = Thread(target=self.run) # Read continuously the output of the process
         self.thread.start()
         self.length = 512
         self.stream = b''
 
+    """
+    readline()
+    Get the next element stored in the queue and return it
+    """
+        
     def readline(self):
         if(self.queue.empty()):
             return b''
@@ -25,6 +35,11 @@ class Process:
             
             return line
 
+    """
+    readstream()
+    read line until the buffer is full
+    """
+        
     def readstream(self):
         self.stream = b''
         
@@ -33,16 +48,26 @@ class Process:
             self.stream += line
     
         return self.stream
-        
+
+    """
+    run
+    read the process output while it is still active and store each line in a queue
+    """
     def run(self):
 
-        out = 'undefined'
+        out = b'undefined'
         
         while(self.p.poll() is None or out != b''):
             out = self.p.stdout.readline()
             self.queue.put(out)
             
         self.endProcess = True
+
+
+    """
+    kill()
+    kill the process
+    """
 
     def kill(self):
         self.p.kill()
